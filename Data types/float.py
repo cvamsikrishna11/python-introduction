8368df91f6d61d488e5c99c567ca035d9c61f2df
vamsi chunduru <vamsi chunduru>
2022-04-16 23:47:48 +0530

    added float data type

---
+++ Data types/float.py
@@ -0,0 +1,14 @@
+# To explain the python float data type
+
+a = 2.5
+print(a)
+print(1.3+2.8)
+b=a*2
+print(b)
+print(-10.7)
+
+# print the type of data, using python keyword called type
+print(type(a))
+print(type(b))
+print(type(11.3))
+print(type(2000.4))
