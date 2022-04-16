0b11bc397b3a2b89db89d6d14a9a1eac6d956f9e
vamsi chunduru <vamsi chunduru>
2022-04-16 23:36:08 +0530

    initial commit

---
+++ int.py
@@ -0,0 +1,13 @@
+# To explain the python string data type
+a = 2
+print(a)
+print(1+2)
+b=a*2
+print(b)
+print(-100)
+
+# print the type of data
+print(type(a))
+print(type(b))
+print(type(1))
+print(type(2))
---
+++ string.py
@@ -0,0 +1,1 @@
+# To explain the python string data type
