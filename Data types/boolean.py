3a179d1cab0550757b911ae29fa3eb8247cd4d5a
vamsi chunduru <vamsi chunduru>
2022-04-16 23:54:49 +0530

    added boolean

---
+++ Data types/boolean.py
@@ -0,0 +1,21 @@
+# To explain the python boolean data type
+
+# observe the difference between string and boolean, string value will have '' or "" symbols surrounded
+# where as here True, False are recognized by Python
+
+a = True
+b = False
+print(a)
+print(b)
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+# print the type of data, using python keyword called type
+print(1 == 1, type(1 == 1))
+x = 5 > 4
+print(x, type(x))
+
+y = 4 > 5
+print(y, type(y))
