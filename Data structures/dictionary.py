fffa9eec5bd17ee7f1cd2e87bb8f1f0903738df0
vamsi chunduru <vamsi chunduru>
2022-04-17 15:52:10 +0530

    list & dict

---
+++ Data structures/dictionary.py
@@ -0,0 +1,105 @@
+# Dictionaries are used to store data values in key:value pairs.
+
+student = {"name": "vamsi", "age": "25 years", "college": "abc-college"}
+fruits = {"apple": 5, "banana": 3.50, "watermelon": 2.75}
+friends = {"class-1": ["david", "ricci"], "class-2": ["john", "matheus"]}
+
+# print dictionary
+print('\n # print dictionary')
+print(student)
+print(fruits)
+print(friends)
+
+# access dictionary items
+print('\n # access dictionary items')
+print(student["name"])
+print(fruits.get("apple"))
+print(friends["class-2"])
+
+# check if key exists in a dictionary
+print('\n # check if key exists in a dictionary')
+if 'watermelon' in fruits:
+    print(fruits['watermelon'])
+else:
+    print('requested key not found')
+
+if 'class' in student:
+    print(student['college'])
+else:
+    print('requested key not found')
+
+
+# check dictionary length
+print('\n # check dictionary length')
+print(len(student))
+print(len(friends))
+
+
+# check the data structure type of these values
+print('\n # check the data structure type of these values')
+print(type(student))
+print(type(fruits))
+
+# check the methods and attributes available for the dictionary variable
+print('\n # check the methods and attributes available for the dictionary variable')
+print(dir(student))
+
+# get the keys of dictionary with keys() method
+print('\n # get the keys of dictionary with keys() method')
+print(student.keys())
+
+# get the values of dictionary with values() method
+print('\n # get the keys of dictionary with values() method')
+print(student.values())
+
+# copy all the dictionary data into another variable
+print('\n # copy all the dictionary data into another variable')
+duplicate_student = student.copy()
+print(student)
+print(duplicate_student)
+
+# modify an existing key value in dictionary
+print('\n # modify an existing key value in dictionary')
+student['name'] = 'vamsi krishna chunduru'
+print(student)
+
+# delete a key from the dictionary
+print('\n # delete a key from the dictionary')
+if 'college' in student:
+    student.pop('college')
+    print(student)
+else:
+    print('key not found')
+
+
+# add a new key value to dictionary
+print('\n # add a new key value to dictionary')
+student['college'] = 'abc-college'
+print(student)
+
+
+# iterate over a dictionary keys
+print('\n # iterate over a dictionary keys')
+for key in student.keys():
+    print(key)
+
+# iterate over a dictionary values
+print('\n # iterate over a dictionary values')
+for val in student.values():
+    print(val)
+
+# iterate over a dictionary key,value pairs
+print('\n # iterate over a dictionary key,value pairs')
+for key,val in student.items():
+    print(key, val)
+
+
+# clear all key-value from dictionary
+print('\n # clear all key-value from dictionary')
+student.clear()
+print(student)
+
+# delete the complete dictionary
+print('\n # delete the complete dictionary')
+del student
+print(student)
--- Data structures/lists.py
+++ Data structures/lists.py
@@ -9,59 +9,89 @@ my_list = [1, "car", 10.40, True]
 
 
 # printing the list items
-print('# printing the list items')
+print('\n# printing the list items')
 print(fruits)
 print(prices)
 print(numbers)
 print(my_list)
 
 # check the data type
-print('# check the data type')
+print('\n# check the data type')
 print(type(fruits))
 print(type(numbers))
 
 
 # nested list
-print('# nested list')
+print('\n# nested list')
 my_custom_list = [fruits, prices, [6, 10, 2]]
 print(my_custom_list)
 
+
+# length of list
+print('\n# length of the list')
+print(len(fruits))
+
+# access a specific element in list
+print('\n# access a specific element in list')
+print(fruits[2])
+print(fruits[0])
+print(fruits[-1])
+print(fruits[-2])
+
+# slicing lists
+print('\n# slicing lists')
+print(fruits[0:2])
+print(fruits[1:])
+
+
 # peform actions on list
 # return attributes and methods of an object
-print('# dir() method')
+print('\n# dir() method')
 print(dir(fruits))
 
 # append items to existing list
-print('# append() method')
+print('\n# append() method')
 fruits.append("kiwi")
 print(fruits)
 
 # sort the list
-print('# sort() the list')
+print('\n# sort() the list')
 fruits.sort()
 print(fruits)
 
-# length of list
-print('# length of the list')
-print(len(fruits))
+# insert item at a particular index
+print('\n# insert item at a particular index')
+fruits.insert(0,'grapes')
+print(fruits)
 
-# access a specific element in list
-print('# access a specific element in list')
-print(fruits[2])
-print(fruits[0])
+# remove an item from the list
+print('\n# remove an item from the list')
+fruits.remove('watermelon')
+print(fruits)
 
 # iterate over all items in the list, one after another
-print('# iterate over all items in the list, one after another')
+print('\n# iterate over all items in the list, one after another')
 for each_item in fruits:
     print(each_item)
 
 # iterate over each item in the list along with item index
-print('# iterate over each item in the list along with item index')
+print('\n# iterate over each item in the list along with item index')
 for index, each_item in enumerate(fruits):
     print(index, each_item)
 
 # list with dictionaries inside
-print('# list with dictionaries inside')
+print('\n# list with dictionaries inside')
 marks = [{"name": "vamsi", "marks": 50}, {"name": "avinash",
                                           "marks": 60}, {"name": "showmik", "marks": 70}, ]
 print(marks)
+
+
+# clear a list
+print('\n # clear a list')
+fruits.clear()
+print(fruits)
+
+# delete the complete list
+print('\n # delete the complete list')
+del fruits
+print(fruits)
