e06131c73aa73e6ce1787798aa2daf5f6c1c3779
vamsi chunduru <vamsi chunduru>
2022-04-17 13:22:48 +0530

    added comments code

---
+++ Data types-Variables-Operators-Keywords-Comments/comments.py
@@ -0,0 +1,16 @@
+"""
+Author: Vamsi
+Team: DevOps
+Infra: Docker deployment
+Dependency: Python3.7, Boto3
+"""
+
+
+
+# below code will calculate the total price of items bought
+items_count = 10
+customer_name = 'vamsi'
+is_bill_paid = False
+price = 15.05
+
+print('toal bill to be paid: ', items_count * price)
