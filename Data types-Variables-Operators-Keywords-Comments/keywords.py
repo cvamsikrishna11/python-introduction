36a51985a05897770d703199c9890a3bdc9b15b9
vamsi chunduru <vamsi chunduru>
2022-04-17 13:11:57 +0530

    python keywords

---
+++ Data types-Variables-Operators-Keywords-Comments/keywords.py
@@ -0,0 +1,33 @@
+#Python keywords are special reserved words that have specific meanings and purposes and can’t be used for anything but those specific purposes. 
+# These keywords are always available—you’ll never have to import them into your code.
+
+
+# print
+print('hiii')
+
+# import keyword
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+# True, False, None
+print(False == 0)
+print(True == 1)
+
+print(True + True + True)
+print(True + False + False)
+
+print(None == 0)
+print(None == [])
+
+
+# is, not, in
+print('a' is 'b')
+print('a' is not 'b')
+print('a' in 'apple')
+print('a' not in 'xyz')
+
+
+
